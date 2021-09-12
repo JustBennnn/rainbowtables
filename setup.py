77841a37779759a9b6a64674c217a86bd1f0d32d
JustBennnnn <justben009@gmail.com>
2021-09-12 20:46:45 +0100

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="rainbowtables",
-    version="1.0.3",
+    version="1.0.4",
     author="JustBen",
     author_email="justben009@gmail.com",
     description="A python library allowing the user to create a rainbowtable.",
